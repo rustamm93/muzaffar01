ab6861f8a899cdd4faf6d360c097b535b199e87e
rustamm93 <rustammamadaminov@gmail.com>
2023-04-27 10:02:01 +0500

    first_step

---
+++ content.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <h2 class="text-center">ҚИШЛОҚ ХЎЖАЛИГИ ИШЧИ ВА ХИЗМАТЧИЛАРИ ЎРТАСИДА СОҒЛОМ ОВҚАТЛАНИШ ҲОЛАТИНИ ЎРГАНИШ УЧУН УСЛУБИЙ TABСИЯЛАР</h2>
+    <form action="" class="mt-3" method="$_POST">
+        <div class="row mb-3">
+            <label for="n1" class="col form-label">Туман/шаҳар ва маҳалланинг идентификация рақами
+Планшетга илова қилинган рўйхатдан туман/шаҳар ва маҳалланинг идентификатсия рақамини киритинг
+            </label>
+            <input type="text" class="col form-control" id="n1">
+        </div>
+        <div class="row mb-3">
+            <label for="n2" class="col form-label">Аҳоли яшайдиган жойнинг номи
+Планшетда марказ ёки аҳоли яшаш жойининг номини киритинг
+            </label>
+            <input type="text" class="col form-control" id="n2">
+        </div>
+        <div class="row mb-3">
+            <label for="n3" class="col form-label">Интервью олувчи/суҳбатдошнинг идентификация рақами
+Планшетга ўз идентификацион рақамингизни киритинг
+            </label>
+            <input type="text" class="col form-control" id="n3">
+        </div>
+        <div class="row mb-3">
+            <label for="n4" class="col form-label">Савономани тўлдириш санаси
+Планшетга сўровнома ҳақиқатда тўлдирилган санани (бугунги сана) киритинг
+кун      .     ой           йил
+            </label>
+            <input type="date" class="col form-control" id="n4">
+        </div>
+    </form>
+</body>
+</html>
